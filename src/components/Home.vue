efdc02dbe24439c84423e347be3bfd4d132bdaea
Peter Goshulak <pgoshulak@gmail.com>
2018-05-20 17:49:18 -0400

    Install and init firebase, vuefire, vuetify

---
+++ src/components/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <pre>
+      {{families}}
+    </pre>
+  </div>
+</template>
+
+<script>
+  import { db } from '../main.js'
+  export default {
+    data() {
+      return {
+        families: []
+      }
+    },
+    firestore() {
+      return {
+        families: db.collection('families')
+      }
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,26 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router.js'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+import VueFire from 'vuefire'
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+import { firebaseConfig } from '../secrets.js'
+
+Vue.use(VueFire)
+Vue.use(Vuetify)
+
+// https://medium.com/vue-mastery/full-stack-vue-js-with-firestore-62e2fe2ec1f3
+const firebaseApp = firebase.initializeApp(firebaseConfig)
+export const db = firebaseApp.firestore()
+db.settings({
+  timestampsInSnapshots: true
+})
 
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
 })
