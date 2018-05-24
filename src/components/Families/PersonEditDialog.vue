e3a7816eedff2ac05d053eb24a85acafd159300e
Peter Goshulak <pgoshulak@gmail.com>
2018-05-23 20:58:38 -0400

    Add delete-person-dialog within edit-person-dialog

--- src/components/Families/PersonEditDialog.vue
+++ src/components/Families/PersonEditDialog.vue
@@ -1,26 +1,45 @@
 <template>
-  <v-dialog id="edit-person-name-dialog" v-model="isOpenData" max-width="400px">
-    <v-card>
-      <v-card-title>
-        <h4>Editing {{this.person.name}}</h4>
-      </v-card-title>
-      <v-card-text>
-        <v-text-field
-          v-model="updatePersonNameInput"
-          append-icon="edit"
-          label="Display Name"
-          hide-details
-          :placeholder="this.person.name"
-          @keyup.enter="updatePerson"
-        ></v-text-field>
-      </v-card-text>
-      <v-card-actions>
-        <v-spacer></v-spacer>
-        <v-btn small outline color="primary" @click="close">Cancel</v-btn>
-        <v-btn small color="primary" @click="updatePerson">Save</v-btn>
-      </v-card-actions>
-    </v-card>
-  </v-dialog>
+  <div>
+    <v-dialog id="edit-person-dialog" v-model="isOpenData" max-width="400px">
+      <v-card>
+        <v-card-title>
+          <h4>Editing {{this.person.name}}</h4>
+        </v-card-title>
+        <v-card-text>
+          <v-text-field
+            v-model="updatePersonNameInput"
+            append-icon="edit"
+            label="Display Name"
+            hide-details
+            :placeholder="this.person.name"
+            @keyup.enter="updatePerson"
+          ></v-text-field>
+        </v-card-text>
+        <v-card-actions>
+          <v-btn small outline color="error" @click="deleteDialogIsOpen=true">Delete</v-btn>
+          <v-spacer></v-spacer>
+          <v-btn small outline color="primary" @click="close">Cancel</v-btn>
+          <v-btn small color="primary" @click="updatePerson">Save</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+    <v-dialog id="delete-person-dialog" v-model="deleteDialogIsOpen" max-width="300px">
+      <v-card>
+        <v-card-title>
+          <h4 class="subheading">Really delete {{this.person.name}}?</h4>
+        </v-card-title>
+        <v-card-text>
+          This cannot be undone
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn small outline color="primary" @click="deleteDialogIsOpen=false">Cancel</v-btn>
+          <v-btn small color="error" @click="deletePerson">Delete</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+    
+  </div>
 </template>
 
 <script>
@@ -30,7 +49,8 @@
     data() {
       return {
         updatePersonNameInput: '',
-        isOpenData: false
+        isOpenData: false,
+        deleteDialogIsOpen: false
       }
     },
     watch: {
@@ -60,6 +80,12 @@
       close() {
         this.$emit('close')
         this.updatePersonNameInput = ''
+      },
+      deletePerson() {
+        db.collection("guests").doc(this.person.id).delete().then(() => {
+          this.deleteDialogIsOpen = false;
+          this.close()
+        })
       }
     },
   }
